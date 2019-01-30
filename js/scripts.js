8858d2b1f9d3e673b1465e7acceff18e1e41628d
veronikahar <veronika.har@hotmail.com>
2019-01-30 16:43:05 -0600

    Repository containing 3 pokemon objects with 3 keys created

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-alert('Hello world');
+var charmeleon={name:'Charmeleon', height:110, abilities:['blaze', 'solar-power']};
+var pikachu={name: 'Pikachu', height:40, abilities:['Static', 'Lightningrod']};
+var psyduck={name: 'Psyduck', height:80, abilities:['Damp', 'Cloud-nine', 'Swift-swim']};
+
+var repository=[charmeleon, pikachu, psyduck];
+for (var i = 0; i < repository.length; i++) {
+  document.write('<div class=pokemon>' +repository[i].name + ' - height: ' + repository[i].height + ' cm.</div>')
+  if (repository[i].height>90) {
+    document.write('- Wow, that\'s big! ')
+  }
+}
