6b77907c10346454ea48ea3219cc3b0482da7e6c
veronikahar <veronika.har@hotmail.com>
2019-01-31 14:22:14 -0600

    JS condition added to have various color scheme depending on the tye of Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
-var charmeleon={name:'Charmeleon', height:110, abilities:['blaze', 'solar-power']};
-var pikachu={name: 'Pikachu', height:40, abilities:['Static', 'Lightningrod']};
-var psyduck={name: 'Psyduck', height:80, abilities:['Damp', 'Cloud-nine', 'Swift-swim']};
+var charmeleon={name:'Charmeleon', height:110, type: 'fire', abilities:['blaze', 'solar-power']};
+var pikachu={name: 'Pikachu', height:40, type:'electric', abilities:['Static', 'Lightningrod']};
+var psyduck={name: 'Psyduck', height:80, type:'water', abilities:['Damp', 'Cloud-nine', 'Swift-swim']};
 
 var repository=[charmeleon, pikachu, psyduck];
 for (var i = 0; i < repository.length; i++) {
   if (repository[i].height>90) {
-    document.write('<div class=pokemon>' +repository[i].name + ' - height: ' + repository[i].height + ' cm. Wow, that\'s big! </div>')
+    var nameHeight=repository[i].name + ' - height: ' + repository[i].height + ' cm. Wow, that\'s big!'
   } else {
-    document.write('<div class=pokemon>' +repository[i].name + ' - height: ' + repository[i].height + ' cm.</div>')
+    var nameHeight=repository[i].name + ' - height: ' + repository[i].height + ' cm.'
+  }
+  if (repository[i].type==='fire'){
+    document.write('<div class="fire">'+ nameHeight + '</div>')
+  } else if (repository[i].type==='electric'){
+    document.write('<div class="electric">'+ nameHeight + '</div>')
+  } else if (repository[i].type==='water'){
+    document.write('<div class="water">'+ nameHeight + '</div>')
+  }else{
+    document.write('<div class="pokemon">'+ nameHeight + '</div>')
   }
 }
