6ea6fb3b6cbeeeafbf8b3d808a413ac9b8761b99
veronikahar <veronika.har@hotmail.com>
2019-02-01 13:37:55 -0600

    Pokemon variables moved as objects into the "repository"

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-var charmeleon={name:'Charmeleon', height:110, type: 'fire', abilities:['blaze', 'solar-power']};
-var pikachu={name: 'Pikachu', height:40, type:'electric', abilities:['Static', 'Lightningrod']};
-var psyduck={name: 'Psyduck', height:80, type:'water', abilities:['Damp', 'Cloud-nine', 'Swift-swim']};
+var repository = [
+  {name:'Charmeleon', height:110, type: 'fire', abilities:['blaze', 'solar-power']},
+  {name: 'Pikachu', height:40, type:'electric', abilities:['Static', 'Lightningrod']},
+  {name: 'Psyduck', height:80, type:'water', abilities:['Damp', 'Cloud-nine', 'Swift-swim']}
+];
 
-var repository=[charmeleon, pikachu, psyduck];
 for (var i = 0; i < repository.length; i++) {
   if (repository[i].height>90) {
     var nameHeight=repository[i].name + ' - height: ' + repository[i].height + ' cm. Wow, that\'s big!'
